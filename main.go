361ec3262324e266ece3ba9db90bd7d191d2864f
Nikhilesh S <s.nikhilesh@gmail.com>
2021-09-06 00:33:39 -0700

    basic ast parse and print!

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"go/parser"
+	"go/token"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println("welcome to the gx compiler ;D")
+	fset := token.NewFileSet()
+	root, err := parser.ParseFile(fset, "examples/basic_1.go", nil, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(root)
 }
