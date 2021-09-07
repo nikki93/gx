fdb7ab9ab60eb05e2fcbff13468b65e713cab93a
Nikhilesh S <s.nikhilesh@gmail.com>
2021-09-07 02:11:56 -0700

    type-checking!

--- examples/basic_1.go
+++ examples/basic_1.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+type Foo struct {
+	X int
+	Y [32]byte
+}
+
 func main() {
+	x := 42
 	println("hello, world!")
+	println(x)
+
+	fmt.Println("hello, world!")
 }
--- main.go
+++ main.go
@@ -1,17 +1,73 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/token"
+	"go/types"
 )
 
-func main() {
-	fmt.Println("welcome to the gx compiler ;D")
-	fset := token.NewFileSet()
-	root, err := parser.ParseFile(fset, "examples/basic_1.go", nil, 0)
+//
+// Errors
+//
+
+func unimpl() {
+	panic("unimplemented")
+}
+
+//
+// Compiler
+//
+
+type Compiler struct {
+	mainFilename string
+
+	info *types.Info
+	pkg  *types.Package
+
+	result bytes.Buffer
+}
+
+// Top-level
+
+func (comp *Compiler) compile() {
+	// Parse
+	fileSet := token.NewFileSet()
+	file, err := parser.ParseFile(fileSet, comp.mainFilename, nil, 0)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(root)
+
+	// Type-check
+	config := &types.Config{}
+	comp.info = &types.Info{
+		Types:      make(map[ast.Expr]types.TypeAndValue),
+		Defs:       make(map[*ast.Ident]types.Object),
+		Uses:       make(map[*ast.Ident]types.Object),
+		Implicits:  make(map[ast.Node]types.Object),
+		Selections: make(map[*ast.SelectorExpr]*types.Selection),
+		Scopes:     make(map[ast.Node]*types.Scope),
+	}
+	comp.pkg, err = config.Check("", fileSet, []*ast.File{file}, comp.info)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Compile
+	fmt.Println(comp.info.Defs)
+}
+
+//
+// Main
+//
+
+func main() {
+	// Compile
+	comp := Compiler{mainFilename: "examples/basic_1.go"}
+	comp.compile()
+
+	// Print
+	fmt.Println(comp.result.String())
 }
