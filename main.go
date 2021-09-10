12f5f74a2954fde73d203ad33b9fd7214c509cb2
Nikhilesh S <s.nikhilesh@gmail.com>
2021-09-09 23:22:03 -0700

    refactored to mostly one big function

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 //
-// Types
+// Compiler
 //
 
 type Compiler struct {
@@ -29,10 +29,6 @@ type Compiler struct {
 	output *bytes.Buffer
 }
 
-//
-// Errors
-//
-
 func (c *Compiler) eprintf(pos token.Pos, format string, args ...interface{}) {
 	fmt.Fprintf(c.errors, "%s: ", c.fileSet.PositionFor(pos, true))
 	fmt.Fprintf(c.errors, format, args...)
@@ -43,9 +39,9 @@ func (c *Compiler) errored() bool {
 	return c.errors.Len() != 0
 }
 
-//
-// Analysis
-//
+func (c *Compiler) writef(format string, args ...interface{}) {
+	fmt.Fprintf(c.output, format, args...)
+}
 
 func (c *Compiler) funcCppDecl(decl *ast.FuncDecl) string {
 	if result, ok := c.funcCppDecls[decl]; ok {
@@ -58,62 +54,7 @@ func (c *Compiler) funcCppDecl(decl *ast.FuncDecl) string {
 	}
 }
 
-func (c *Compiler) analyze() {
-	// Parse
-	c.fileSet = token.NewFileSet()
-	for _, filePath := range c.filePaths {
-		file, err := parser.ParseFile(c.fileSet, filePath, nil, 0)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		c.files = append(c.files, file)
-	}
-
-	// Type-check
-	config := types.Config{}
-	c.types = &types.Info{
-		Types:      make(map[ast.Expr]types.TypeAndValue),
-		Defs:       make(map[*ast.Ident]types.Object),
-		Uses:       make(map[*ast.Ident]types.Object),
-		Implicits:  make(map[ast.Node]types.Object),
-		Selections: make(map[*ast.SelectorExpr]*types.Selection),
-		Scopes:     make(map[ast.Node]*types.Scope),
-	}
-	if _, err := config.Check("", c.fileSet, c.files, c.types); err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
-//
-// Writing
-//
-
-func (c *Compiler) writef(format string, args ...interface{}) {
-	fmt.Fprintf(c.output, format, args...)
-}
-
-func (c *Compiler) write() {
-	// Preamble
-	c.writef("#include \"../preamble.hh\"\n")
-
-	// Function declarations
-	c.writef("\n\n")
-	for _, file := range c.files {
-		for _, decl := range file.Decls {
-			if decl, ok := decl.(*ast.FuncDecl); ok {
-				c.writef("%s\n", c.funcCppDecl(decl))
-			}
-		}
-	}
-}
-
-//
-// Top-level
-//
-
-func (c *Compiler) init() {
+func (c *Compiler) compile() {
 	// Collect file paths from directory
 	if len(c.filePaths) == 0 && c.directoryPath != "" {
 		fileInfos, err := ioutil.ReadDir(c.directoryPath)
@@ -132,13 +73,43 @@ func (c *Compiler) init() {
 	// Initialize buffers
 	c.errors = &bytes.Buffer{}
 	c.output = &bytes.Buffer{}
-}
 
-func (c *Compiler) compile() {
-	c.init()
-	c.analyze()
-	if !c.errored() {
-		c.write()
+	// Parse
+	c.fileSet = token.NewFileSet()
+	for _, filePath := range c.filePaths {
+		file, err := parser.ParseFile(c.fileSet, filePath, nil, 0)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		c.files = append(c.files, file)
+	}
+
+	// Type-check
+	c.types = &types.Info{
+		Types:      make(map[ast.Expr]types.TypeAndValue),
+		Defs:       make(map[*ast.Ident]types.Object),
+		Uses:       make(map[*ast.Ident]types.Object),
+		Implicits:  make(map[ast.Node]types.Object),
+		Selections: make(map[*ast.SelectorExpr]*types.Selection),
+		Scopes:     make(map[ast.Node]*types.Scope),
+	}
+	if _, err := (&types.Config{}).Check("", c.fileSet, c.files, c.types); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Write preamble
+	c.writef("#include \"../preamble.hh\"\n")
+
+	// Write function declarations
+	c.writef("\n\n")
+	for _, file := range c.files {
+		for _, decl := range file.Decls {
+			if decl, ok := decl.(*ast.FuncDecl); ok {
+				c.writef("%s\n", c.funcCppDecl(decl))
+			}
+		}
 	}
 }
 
