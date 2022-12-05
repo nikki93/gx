93daf4238286f357a61d664da4b85558675c8747
Nikhilesh S <s.nikhilesh@gmail.com>
2022-12-05 01:11:27 -0800

    fix gxsl test for previous change

--- example/glsl/main.gx.go
+++ example/glsl/main.gx.go
@@ -11,32 +11,32 @@ func GLSL(shaderFunc interface{}) (result string) { return }
 // Built-ins
 //
 
-//gx:glsl:extern vec2
+//gxsl:extern vec2
 type Vec2 struct {
 	X, Y float64
 }
 
-//gx:glsl:extern vec4
+//gxsl:extern vec4
 type Vec4 struct {
 	X, Y, Z, W float64
 }
 
-//gx:glsl:extern -
+//gxsl:extern -
 func (v Vec4) Negate() Vec4
 
-//gx:glsl:extern *
+//gxsl:extern *
 func (v Vec4) Multiply(u Vec4) Vec4
 
-//gx:glsl:extern *
+//gxsl:extern *
 func (v Vec4) Scale(f float64) Vec4
 
-//gx:glsl:extern dot
+//gxsl:extern dot
 func (v Vec4) DotProduct(u Vec4) float64
 
-//gx:glsl:extern sampler2D
+//gxsl:extern sampler2D
 type Sampler2D struct{}
 
-//gx:glsl:extern texture2D
+//gxsl:extern texture2D
 func Texture2D(sampler Sampler2D, coord Vec2) Vec4
 
 var gl_FragColor Vec4
@@ -55,7 +55,7 @@ type RedTextureParams struct {
 	Texture0   Sampler2D
 }
 
-//gx:glsl:entry
+//gxsl:entry
 func redTextureShader(uniforms RedTextureParams, varyings Varyings) {
 	result := Vec4{-1, -0.2, -0.2, -1}.Negate()
 
